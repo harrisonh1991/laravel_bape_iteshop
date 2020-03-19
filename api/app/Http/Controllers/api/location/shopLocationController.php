5410f21935970a72665337ae6f883186db6976de
harrisonh1991 <kye0038883@gmail.com>
2020-03-19 09:14:17 +0800

    update

--- api/app/Http/Controllers/api/location/shopLocationController.php
+++ api/app/Http/Controllers/api/location/shopLocationController.php
@@ -7,26 +7,6 @@ use Illuminate\Http\Request;
 
 class shopLocationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
@@ -36,29 +16,6 @@ class shopLocationController extends Controller
      */
     public function show($id)
     {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -14,10 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::get('user/{id}', function($id){
+    return 'User '.$id;
 });
 
-Route::middleware('auth:api')-> group(function(){
-    
+Route::get('foo', function(){
+    return 'hi';
 });
+
+//Route::get('v1/getLocation', 'shopLocationController');
